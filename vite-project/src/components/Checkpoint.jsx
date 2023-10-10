6420b0af6249ab7d2b0f0b7d31cfb0abb136ce6b
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-10-10 19:57:04 +0200

    first commit

---
+++ vite-project/src/components/Checkpoint.jsx
@@ -0,0 +1,18 @@
+import "./Checkpoint.scss";
+import { FaRegCheckCircle } from "react-icons/fa";
+import React, { useState, useEffect, useRef } from "react";
+
+export const Checkpoint = () => {
+  return (
+    <>
+      <a
+        href="https://github.com/MLT93/checkpoint-3"
+        rel="noreferrer"
+        target="_blank"
+        className="checkpoint-title">
+        <FaRegCheckCircle className="approved" />
+        <span>Checkpoint</span>
+      </a>
+    </>
+  );
+};
